7742d8b8d5aa5a585330c1c0ebe8f5a3da3be0d2
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-12-14 21:39:25 -0300

    Eliminar archivos de contexto de autenticación, componentes de botones y servicios de usuarios y almacenamiento

--- src/services/auth.ts
+++
@@ -1,4 +0,0 @@
-export async function fakeLogin(email: string, password: string) {
-  await new Promise((resolve) => setTimeout(resolve, 700)); // simulación delay
-  return email === "admin@demo.com" && password === "123456";
-}
